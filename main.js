278d92393e7f6e10bfd68e396716110b079a4603
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2024-07-29 09:38:20 +0200

    Create function to generatre random choice for computer

---
+++ main.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let rand;
+    let computerChoice;
+    rand = Math.random();
+    if (rand<0.33) {
+        computerChoice="Rock";
+    } else if (rand<0.66) {
+        computerChoice="Paper";
+    } else {
+        computerChoice="Scissor";
+    }
+    return computerChoice;
+}
+console.log(getComputerChoice());
