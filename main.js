df980bb779dd0aa71162a62c3b2ed972090dbb77
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2024-07-30 09:37:06 +0200

    add function that determines the winner, increments their score and logs the result to the console

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let rand;
     let computerChoice;
@@ -15,6 +18,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     userInput = prompt("Select your choice: Rock, paper or scissors").toLowerCase();
+    // Checks to see if input is valid. Otherwise alert user
     if (userInput === "rock" || userInput === "paper" || userInput === "scissor") {
         return userInput;
     } else {
@@ -22,4 +26,45 @@ function getHumanChoice() {
     }
 }
 
-getHumanChoice();
+function playround(humanChoice, computerChoice) {
+    let winner;
+    if (humanChoice == computerChoice) {
+        winner = "tie";
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            winner = "computer"
+        } else {
+            winner = "human";
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissor") {
+            winner = "computer";
+        } else {
+            winner = "human";
+        }
+    } else if (humanChoice === "scissor") {
+        if (computerChoice === "rock") {
+            winner = "computer";
+        } else {
+            winner = "human"
+        }
+    }
+    
+    if (winner === "human") {
+        console.log("You win!")
+        humanScore++;
+    } else {
+        console.log("You lose")
+        computerScore++;
+    }
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playround(humanSelection,computerSelection);
